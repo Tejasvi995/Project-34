c0de80eace9dae4309c30cc6a403c227b5dd5f68
Tejasvi995 <75204569+Tejasvi995@users.noreply.github.com>
2021-03-17 14:32:53 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,23 @@
+class Hero {
+    constructor(x,y,r) {
+      var options = {
+          density:3,
+          frictionAir:0.005
+      }
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.image = loadImage("images/Superhero-01.png")
+      this.body = Bodies.circle(this.x,this.y,this.r,options);
+      World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        //rotate(angle);
+        imageMode(CENTER)
+        image(this.image,0,0,this.r*2,this.r*2);
+        pop();
+    }
+  }
---
+++ Monster.js
@@ -0,0 +1,24 @@
+class Monster {
+    constructor(x,y,r) {
+      var options = {
+        restitution:0.5,
+        density: 0.1,
+        friction:0.3
+      }
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.image = loadImage("images/Monster-01.png")
+      this.body = Bodies.circle(this.x,this.y,this.r,options);
+      World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        //rotate(angle);
+        imageMode(CENTER)
+        image(this.image,0,0,this.r*2,this.r*2);
+        pop();
+    }
+  }
